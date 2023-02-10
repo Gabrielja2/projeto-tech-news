ce0068c4364f9e3948aa82deda4275716edbbeb6
Gabriel <gabrielja2antunes@gmail.com>
2023-02-09 21:10:44 -0300

    🚀 features: create function top_5_categories

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,14 @@
-# Requisito 10
+from tech_news.database import find_news
+from collections import Counter
+
+
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    try:
+        finded_news = find_news()
+        categories = [news["category"] for news in finded_news]
+        categories_most_common = Counter(sorted(categories)).most_common(5)
+
+        return [category[0] for category in categories_most_common]
+
+    except ValueError:
+        return []
